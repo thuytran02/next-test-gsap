306a763cd7556577ba191342cd2d394860182598
thuytran02 <thuytt@kaopiz.com>
2023-12-22 09:50:00 +0700

    add logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,163 @@
-import Image from 'next/image'
+"use client";
+import { useEffect, useRef, useState } from "react";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useIsomorphicLayoutEffect } from "@/src/helper/isomorphicEffect";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+  const main = useRef<HTMLDivElement>(null);
+
+  const initialRef = useRef(false);
+
+  useEffect(() => {
+    initialRef.current = false;
+  }),
+    [];
+
+  useEffect(() => {
+    const container = document.querySelector(".container");
+    const sections = gsap.utils.toArray(".container section");
+    const texts = gsap.utils.toArray(".anim");
+    const mask = document.querySelector(".mask");
+
+    let scrollTween = gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".container",
+        pin: true,
+        scrub: 1,
+        end: "+=3000",
+        //snap: 1 / (sections.length - 1),
+        markers: true,
+      },
+    });
+
+    console.log(1 / (sections.length - 1));
+
+    //Progress bar animation
+
+    // gsap.to(mask, {
+    //   width: "100%",
+    //   scrollTrigger: {
+    //     trigger: ".wrapper",
+    //     start: "top left",
+    //     scrub: 1,
+    //   },
+    // });
+
+    // whizz around the sections
+    sections.forEach((section) => {
+      // grab the scoped text
+      // let text = section.querySelectorAll(".anim");
+      // bump out if there's no items to animate
+      // if (text.length === 0) return;
+      // do a little stagger
+      // gsap.from(text, {
+      //   y: -130,
+      //   opacity: 0,
+      //   duration: 2,
+      //   ease: "elastic",
+      //   stagger: 0.1,
+      //   scrollTrigger: {
+      //     trigger: section,
+      //     containerAnimation: scrollTween,
+      //     start: "left center",
+      //     markers: true
+      //   }
+      // });
+    });
+  }, []);
+
+  // useIsomorphicLayoutEffect(() => {
+  //   const ctx = gsap.context((self) => {
+  //     const container = main.current;
+  //     const sections = gsap.utils.toArray(".container section");
+  //     const texts = gsap.utils.toArray(".anim");
+  //     const mask = document.querySelector(".mask");
+
+  //     let scrollTween = gsap.to(sections, {
+  //       xPercent: -100 * (sections.length - 1),
+  //       ease: "none",
+  //       scrollTrigger: {
+  //         trigger: ".container",
+  //         pin: true,
+  //         scrub: 1,
+  //         end: "+=3000",
+  //         //snap: 1 / (sections.length - 1),
+  //         markers: true,
+  //       },
+  //     });
+
+  //     console.log(1 / (sections.length - 1));
+  //   }, main);
+  //   return () => ctx.revert();
+  // }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className=" w-full text-black">
+      <div className="h-[20vh] w-full">this is the first move</div>
+      <div className="wrapper">
+        <div className="container scrollx" ref={main}>
+          <section className="sec1 pin">
+            <span>Advanced</span>
+            <h1>Signify Elegance</h1>
+            <div className="col">
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Egestas euismod nec sit
+                sed massa turpis in. Sit praesent arcu leo lectus pellentesque.
+                Ornare elit orci morbi volutpat. Ut fermentum lorem morbi quis
+                risus amet urna. Urna egestas lorem.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Egestas euismod nec sit
+                sed massa turpis in. Sit praesent arcu leo lectus pellentesque.
+                Ornare elit orci morbi volutpat. Ut fermentum lorem morbi quis
+                risus amet urna. Urna egestas lorem.
+              </p>
+            </div>
+          </section>
+          <section className="sec2 pin">
+            <span className="anim">Advanced</span>
+            <h1 className="anim">Signify Elegance</h1>
+            <div className="col anim">
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Egestas euismod nec sit
+                sed massa turpis in. Sit praesent arcu leo lectus pellentesque.
+                Ornare elit orci morbi volutpat. Ut fermentum lorem morbi quis
+                risus amet urna. Urna egestas lorem.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Egestas euismod nec sit
+                sed massa turpis in. Sit praesent arcu leo lectus pellentesque.
+                Ornare elit orci morbi volutpat. Ut fermentum lorem morbi quis
+                risus amet urna. Urna egestas lorem.
+              </p>
+            </div>
+          </section>
+          <section className="sec3 pin">
+            <span className="anim">Advanced</span>
+            <h1 className="anim">Signify Elegance</h1>
+            <div className="col anim">
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Egestas euismod nec sit
+                sed massa turpis in. Sit praesent arcu leo lectus pellentesque.
+                Ornare elit orci morbi volutpat. Ut fermentum lorem morbi quis
+                risus amet urna. Urna egestas lorem.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Egestas euismod nec sit
+                sed massa turpis in. Sit praesent arcu leo lectus pellentesque.
+                Ornare elit orci morbi volutpat. Ut fermentum lorem morbi quis
+                risus amet urna. Urna egestas lorem.
+              </p>
+            </div>
+          </section>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
